2f07fcb5566e819a4685d385a8a291489840d776
bbouley <b.bouley@hotmail.co.uk>
2016-03-09 10:43:06 -0700

    split up exercises and added in stretch file

---
+++ stretch.js
@@ -0,0 +1,35 @@
+// Strech Goals
+
+// HEDS ER TALES?
+// 1. Use the following variable for your coin flip action:
+       var coin = Math.floor(Math.random() * 2);
+//     We're using Math.random again, along with Math.floor.
+//     Remember, Math.random gives you a random number between 0 and 1.
+//     Calling Math.floor on that number will truncate the decimal, and give you a
+//     random number within the bounds of your array. (In this case, our array will only contain two items.)
+// 2. Use a while loop to keep flipping the coin until you get tails.
+
+// 3. FIZZ BUZZ
+// Write a program that uses console.log to print each number up to 100, with a couple exceptions...
+// If the number is divisible by 3, print "Fizz" instead of the number.
+// If the number is divisible by 5, print "Buzz" instead of the number.
+// If the number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
+
+
+// CHESS BOARD
+// 4. Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
+// At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+
+//Passing this string to console.log should show something like this:
+
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+
+// When you have a program that generates this pattern, define a variable size = 8 and change the program
+// so that it works for any size, outputting a grid of the given width and height.
