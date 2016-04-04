76064ec9c6f63afe4234bb752d17f4dcfeed85bd
Jenny Allar <jennyallar@gmail.com>
2016-04-04 14:47:56 -0600

    removing crappy typeOf from project

--- main.js
+++ main.js
@@ -35,10 +35,7 @@ var numArray = [2, 17, 9, 24, 8];
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,];
 var primeArray = [];
 
-// 9. Prompt a user to pick either a number or a word. Create a script that alerts the user whether they chose a number or a word.
-// Hint: Google 'typeof'
-
-// 10. Write a loop that outputs the following to the console:
+// 9. Write a loop that outputs the following to the console:
 // #
 // ##
 // ###
@@ -52,22 +49,22 @@ var primeArray = [];
 // We're going to create a paper, rock, scissors game that prompts you for your choice and allows the computer to
 // randomly choose. You will use an alert to define the winner.
 
-// 11. Use a variable called "human" to prompt the user to type their choice.
-// 12. Define an array called "choices" that consists of "paper", "rock", and "scissors".
-// 13. Define a variable called "computer" and use Math.random to allow the computer to randomly select a number.
+// 10. Use a variable called "human" to prompt the user to type their choice.
+// 11. Define an array called "choices" that consists of "paper", "rock", and "scissors".
+// 12. Define a variable called "computer" and use Math.random to allow the computer to randomly select a number.
 //    For reference:
 //    Math.random gives you a random number between 0 and 1, which is different each time you call it.
-// 14. Let's start our conditional statement. Start by reassigning the computer variable to "rock" if the random number
+// 13. Let's start our conditional statement. Start by reassigning the computer variable to "rock" if the random number
 //    falls between 0 and .33. Run the code until the console prints out "rock" instead of the number.
 //    In English, it will look like this:
 //      var computer = my Math.random code here
 //       if the number is between 0 and 33
 //          computer = "rock"
 //       log computer to the console
-// 15. Now add the following conditionals to the same statement:
+// 14. Now add the following conditionals to the same statement:
 //    If the random number is between .34 and .66, set the computer variable to "paper".
 //    If the random number is between .67 and 1, set the computer variable to "scissors". (Who the hell seriously chooses scissors first?)
-// 16. Using both "human", begin another conditional statement. At this time, leave the statements blank.
+// 15. Using both "human", begin another conditional statement. At this time, leave the statements blank.
 //    In English, it will look like this:
 //      if human is "rock"
 //        leave this blank
@@ -75,24 +72,24 @@ var primeArray = [];
 //        leave this blank
 //      else
 //        leave this blank
-// 17. Now inside of each condition, we need to compare the "human" variable to the "computer" variable,
+// 16. Now inside of each condition, we need to compare the "human" variable to the "computer" variable,
 //    then print to the console who won the game.
-// 18. Give yourself a high five for completing your first javascript game!
-// 19. Use the game you made above and rework it using a switch statement.
-// 20. What happens if your user enters something other than "rock", "paper", or "scissors?". Change your default case
+// 17. Give yourself a high five for completing your first javascript game!
+// 18. Use the game you made above and rework it using a switch statement.
+// 19. What happens if your user enters something other than "rock", "paper", or "scissors?". Change your default case
 //     to print a snarky message to the console if the input doesn't match any of the options.
 
 
 // HEDS ER TALES?
-// 21. Use the following variable for your coin flip action:
+// 20. Use the following variable for your coin flip action:
        var coin = Math.floor(Math.random() * 2);
 //     We're using Math.random again, along with Math.floor.
 //     Remember, Math.random gives you a random number between 0 and 1.
 //     Calling Math.floor on that number will truncate the decimal, and give you a
 //     random number within the bounds of your array. (In this case, our array will only contain two items.)
-// 22. Use a while loop to keep flipping the coin until you get tails.
+// 21. Use a while loop to keep flipping the coin until you get tails.
 
-// 23. FIZZ BUZZ
+// 22. FIZZ BUZZ
 // Write a program that uses console.log to print each number up to 100, with a couple exceptions...
 // If the number is divisible by 3, print "Fizz" instead of the number.
 // If the number is divisible by 5, print "Buzz" instead of the number.
@@ -100,7 +97,7 @@ var primeArray = [];
 
 
 // CHESS BOARD
-// 24. Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
+// 23. Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
 // At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
 
 //Passing this string to console.log should show something like this:
@@ -116,5 +113,3 @@ var primeArray = [];
 
 // When you have a program that generates this pattern, define a variable size = 8 and change the program
 // sso that it works for any size, outputting a grid of the given width and height.
-
-
