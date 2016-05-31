3004ee9d07a2d350c40cdcf689b8739e2e4ce468
Jenny Allar <jennyallar@gmail.com>
2016-05-30 22:57:51 -0600

    updating readme and splitting tracks

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # week2-project2
 
-Use the Week 2 - Lecture #2 material to assist you in completing this assignment. 
-You will need to post this completed project on your own GitHub account before the next class.
+Use the Week 2 - Lecture #2 material to assist you in completing this assignment.
 
 If you have any questions, please reach out to your classmates or instructor.
--- main.js
+++ main.js
@@ -1,4 +1,32 @@
-// JavaScript Looping Exercise
+/*
+=======================================================
+
+** Week 2 - Project 2 **
+*** Looping & Conditionals ***
+
+Below are a number of problems for you to solve
+using JS. The JS code can be written below each
+problem and the result can be logged to the console.
+
+In order to make the results easy to read,
+please use console.log("Question #") to declare the
+problem number being solved followed by console.log(yourAnswer) after each question.
+
+For any methods that we didn't cover, please refer to the Loops and Iteration Documentation.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
+
+or If...Else Documentation
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+
+or While Loop Documentation
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while
+
+
+BASIC TRACK: 1-15
+ADVANCED TRACK: 16-20
+=======================================================
+*/
+
 
 // 1. Output each item in the following Array to your console:
 var livingRoom = ['couch', 'lamp', 'rug', 'shelf'];
@@ -8,34 +36,20 @@ var livingRoom = ['couch', 'lamp', 'rug', 'shelf'];
 // 3. Using a similar loop, log numbers 75 to 100, only in increments of five.
 
 // 4. Write a while loop that logs "This is how a professional loops." to the console 5 times.
-//     Use this as an example:
-//        var counter = 0;
-//        var num = 2;
 
-//        while (num < 1000) {
-//        num += num;
-//        counter++;
-//        }
+// 5. Separately, use both a for loop and while loop to do the same thing.
+//     Print out the sentence "At home, I have _____ cats."
+//     The numbers should range from 10 to 110, in increments of 25.
 
-//        console.log(counter);
-
-// 5. Write a conditional statement to find the largest of the numbers in the array provided.
-var largestNum = [-5, -2, -6, 0, -1]
-
-// 6. Separately, use both a for loop and while loop to do the same thing.
-//     Print out the sentence "At home, I have _____ cats." Use the number from your counter to fill in the number.
-//     The numbers should range from 10 to 100, in increments of 25.
-
-// 7. Given the following Array, console log 'Even' if the number is even, 'Even and greater than 10' if the
+// 6. Given the following Array, console log 'Even' if the number is even, 'Even and greater than 10' if the
 //    number is even and greater than 10, and 'Odd' if the number is odd.
 //    HINT: Google 'remainder operator'
 var numArray = [2, 17, 9, 24, 8];
 
-// 8. Given the following Array, create variable numThrees with the value [13, 23, 33, 43, 53, 63, 73]
+// 7. Using the following Array, create variable called numThrees with the value [13, 23, 33, 43, 53, 63, 73]
 var numArray = [13, 15, 17, 23, 25, 27, 33, 35, 37, 43, 45, 47, 53, 55, 57, 63, 65, 67, 73, 75, 77]
-var numThrees = [];
 
-// 9. Write a loop that outputs the following to the console:
+// 8. Write a loop that outputs the following to the console:
 // #
 // ##
 // ###
@@ -44,60 +58,46 @@ var numThrees = [];
 // ######
 // #######
 
+// 9. FIZZ BUZZ
+// Write a program that uses console.log to print each number up to 100, with a couple exceptions...
+// If the number is divisible by 3, print "Fizz" instead of the number.
+// If the number is divisible by 5, print "Buzz" instead of the number.
+// If the number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
 
 // RO SHAM BO!
 // We're going to create a paper, rock, scissors game that prompts you for your choice and allows the computer to
 // randomly choose. You will use an alert to define the winner.
 
 // 10. Use a variable called "human" to prompt the user to type their choice.
-// 11. Define an array called "choices" that consists of "paper", "rock", and "scissors".
-// 12. Define a variable called "computer" and use Math.random to allow the computer to randomly select a number.
-//    For reference:
-//    Math.random gives you a random number between 0 and 1, which is different each time you call it.
-// 13. Let's start our conditional statement. Start by reassigning the computer variable to "rock" if the random number
-//    falls between 0 and .33. Run the code until the console prints out "rock" instead of the number.
-//    In English, it will look like this:
-//      var computer = my Math.random code here
-//       if the number is between 0 and 33
-//          computer = "rock"
-//       log computer to the console
-// 14. Now add the following conditionals to the same statement:
-//    If the random number is between .34 and .66, set the computer variable to "paper".
-//    If the random number is between .67 and 1, set the computer variable to "scissors". (Who the hell seriously chooses scissors first?)
-// 15. Using both "human", begin another conditional statement. At this time, leave the statements blank.
-//    In English, it will look like this:
-//      if human is "rock"
-//        leave this blank
-//      if human is "paper"
-//        leave this blank
-//      else
-//        leave this blank
-// 16. Now inside of each condition, we need to compare the "human" variable to the "computer" variable,
-//    then print to the console who won the game.
-// 17. Give yourself a high five for completing your first javascript game!
-// 18. Use the game you made above and rework it using a switch statement.
-// 19. What happens if your user enters something other than "rock", "paper", or "scissors?". Change your default case
+// 11. Define a variable called "computer" and use Math.random to allow the computer to randomly select a number.
+//     For reference:
+//     Math.random gives you a random number between 0 and 1, which is different each time you call it.
+// 12. Let's start our conditional statement.
+//     If the random computer number falls between 0 and .33, the computer is "rock"
+//     If the random computer number falls between .34 and .66, the computer is "paper"
+//     If the random computer number falls between .67 and 1, the computer is "scissors"
+// 13. Using both "human" and the computer choice, begin another conditional statement to compare them.
+// 14. After comparing, determine who has won; the computer or the human!
+// 15. Give yourself a high five for completing your first javascript game!
+// BONUS: What happens if your user enters something other than "rock", "paper", or "scissors?". Change your default case
 //     to print a snarky message to the console if the input doesn't match any of the options.
 
 
-// HEDS ER TALES?
-// 20. Use the following variable for your coin flip action:
+// ADVANCED TRACK
+// 16. Write a conditional statement to find the largest of the numbers in the array provided.
+var largestNum = [-5, -2, -6, 0, -1]
+
+// HEADS OR TAILS?
+// 17. Use the following variable for your coin flip action:
        var coin = Math.floor(Math.random() * 2);
 //     We're using Math.random again, along with Math.floor.
 //     Remember, Math.random gives you a random number between 0 and 1.
 //     Calling Math.floor on that number will truncate the decimal, and give you a
 //     random number within the bounds of your array. (In this case, our array will only contain two items.)
-// 21. Use a do/while loop to keep flipping the coin until you get tails.
-
-// 22. FIZZ BUZZ
-// Write a program that uses console.log to print each number up to 100, with a couple exceptions...
-// If the number is divisible by 3, print "Fizz" instead of the number.
-// If the number is divisible by 5, print "Buzz" instead of the number.
-// If the number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
-
+// 18. Use a do/while loop to keep flipping the coin until you get tails.
 
 // CHESS BOARD
-// 23. Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
+// 19. Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines.
 // At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
 
 //Passing this string to console.log should show something like this:
@@ -111,5 +111,5 @@ var numThrees = [];
 //  # # # #
 // # # # #
 
-// When you have a program that generates this pattern, define a variable size = 8 and change the program
+// 20. When you have a program that generates this pattern, define a variable size = 8 and change the program
 // sso that it works for any size, outputting a grid of the given width and height.
